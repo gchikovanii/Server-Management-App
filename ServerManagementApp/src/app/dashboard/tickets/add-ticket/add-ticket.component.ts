adedf43471d9eb796377dbc0571a82acfe43af5f
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-05 22:33:52 +0400

    second update

--- ServerManagementApp/src/app/dashboard/tickets/add-ticket/add-ticket.component.ts
+++ ServerManagementApp/src/app/dashboard/tickets/add-ticket/add-ticket.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
-  selector: 'app-add-ticket',
-  standalone: true,
-  imports: [ButtonComponent],
-  templateUrl: './add-ticket.component.html',
-  styleUrl: './add-ticket.component.css'
+    selector: 'app-add-ticket',
+    standalone: true,
+    templateUrl: './add-ticket.component.html',
+    styleUrl: './add-ticket.component.css',
+    imports: [ButtonComponent, ControlComponent]
 })
 export class AddTicketComponent {
 
